f8bc96d2738a3debf4f330cf447d094394209825
dmadridy <davidmadridpathway@gmail.com>
2026-01-12 14:42:42 -0500

    Configured yml file for vitest and installed playwright agents

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,3 @@
-import Counter from "@/components/counter";
-
 export default function Dashboard() {
-  return (
-    <div className="flex flex-1 items-center justify-center">
-      <Counter />
-    </div>
-  );
+  return <div className="flex flex-1 items-center justify-center"></div>;
 }
